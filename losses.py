7330f4849acf4cf541b5b0d2d81dacd56b6b4ec9
irina <rakotoarisedy.irina@gmail.com>
2022-05-03 15:30:01 +0200

    Addong of BCE + Dice loss

--- losses.py
+++ losses.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.losses import BinaryCrossentropy
 
 from metrics import dice_coeff, tversky
 
@@ -18,6 +19,11 @@ def weighted_cross_entropy(targets, inputs, beta=0.2):
     weight_b = 1 - tf.cast(targets, tf.float32)
     o = (tf.math.log1p(tf.exp(-tf.abs(inputs))) + tf.nn.relu(-inputs)) * (weight_a + weight_b) + inputs * weight_b
     return tf.reduce_mean(o)
+
+
+def bce_dice_loss(targets, inputs):
+    loss = BinaryCrossentropy() + dice_loss(targets, inputs)
+    return loss
 
 
 def wce_dice_loss(targets, inputs):
